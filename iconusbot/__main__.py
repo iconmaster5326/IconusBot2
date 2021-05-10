4d8255a6cc5e45fefe69f302efec311025d8254e
iconmaster5326 <crazybonesj@yahoo.com>
2021-05-09 20:30:55 -0400

    Added rerollability to fallout 2d20 helpers

--- iconusbot/__main__.py
+++ iconusbot/__main__.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import discord
 from discord import activity
+from discord import message
 from iconusbot.roll import DiceRollError
 import sys
 import typing
@@ -30,7 +31,7 @@ async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
 
-class DamageDie(enum.Enum):
+class DamageDieFace(enum.Enum):
     HIT = (1,)
     DOUBLE_HIT = (2,)
     MISS = (3, 4)
@@ -38,27 +39,85 @@ class DamageDie(enum.Enum):
 
 
 DMG_DIE_TO_EMOJI = {
-    DamageDie.HIT: "<:die1:840298111863095316>",
-    DamageDie.DOUBLE_HIT: "<:die2:840298111955501086>",
-    DamageDie.MISS: "<:die34:840298111884853268>",
-    DamageDie.SPECIAL: "<:die56:840298112043712532>",
+    DamageDieFace.HIT: "<:die1:840298111863095316>",
+    DamageDieFace.DOUBLE_HIT: "<:die2:840298111955501086>",
+    DamageDieFace.MISS: "<:die34:840298111884853268>",
+    DamageDieFace.SPECIAL: "<:die56:840298112043712532>",
 }
 
 DMG_DIE_TO_DAMAGE = {
-    DamageDie.HIT: 1,
-    DamageDie.DOUBLE_HIT: 2,
-    DamageDie.MISS: 0,
-    DamageDie.SPECIAL: 1,
+    DamageDieFace.HIT: 1,
+    DamageDieFace.DOUBLE_HIT: 2,
+    DamageDieFace.MISS: 0,
+    DamageDieFace.SPECIAL: 1,
 }
 
 DIE_ROLL_TO_DMG_DIE = {
-    1: DamageDie.HIT,
-    2: DamageDie.DOUBLE_HIT,
-    3: DamageDie.MISS,
-    4: DamageDie.MISS,
-    5: DamageDie.SPECIAL,
-    6: DamageDie.SPECIAL,
+    1: DamageDieFace.HIT,
+    2: DamageDieFace.DOUBLE_HIT,
+    3: DamageDieFace.MISS,
+    4: DamageDieFace.MISS,
+    5: DamageDieFace.SPECIAL,
+    6: DamageDieFace.SPECIAL,
 }
+
+DMG_DIE_TO_REROLL_WEIGHT = {
+    DamageDieFace.HIT: 2,
+    DamageDieFace.DOUBLE_HIT: 3,
+    DamageDieFace.MISS: 1,
+    DamageDieFace.SPECIAL: 3,
+}
+
+
+class DamageDice:
+    def __init__(self, dice: int) -> None:
+        self.dice = [DamageDie(self) for _ in range(dice)]
+        self.refresh_info()
+
+    def refresh_info(self):
+        self.damage = sum(die.damage for die in self.dice)
+        self.special = sum(die.special for die in self.dice)
+
+    def miss_fortune(self):
+        nonrerolled = [die for die in self.dice if not die.rerolled]
+        if len(nonrerolled) == 0:
+            return
+        lowest_die = min(
+            nonrerolled, key=lambda die: DMG_DIE_TO_REROLL_WEIGHT[die.face]
+        )
+        lowest_die.reroll(True)
+        self.refresh_info()
+
+    def __repr__(self) -> str:
+        return "%s\nTotal: **%s** damage and **%s** effect activations!" % (
+            "".join(str(die) for die in self.dice),
+            self.damage,
+            self.special,
+        )
+
+
+class DamageDie:
+    def __init__(self, dice: DamageDice) -> None:
+        self.dice = dice
+        self.reroll(False)
+
+    def reroll(self, rerolled: bool):
+        self.rerolled = rerolled
+        self.roll = random.randint(1, 6)
+        self.face = DIE_ROLL_TO_DMG_DIE[self.roll]
+        self.damage = DMG_DIE_TO_DAMAGE[self.face]
+        self.special = 1 if self.face == DamageDieFace.SPECIAL else 0
+        self.emoji = DMG_DIE_TO_EMOJI[self.face]
+
+    def __repr__(self) -> str:
+        result = self.emoji
+        if self.rerolled:
+            result = "(%s)" % result
+        return result
+
+
+message_to_damage: typing.Dict[int, DamageDice] = {}
+FOUR_LEAF_CLOVER: str = "🍀"
 
 
 @client.command(
@@ -70,31 +129,123 @@ Parameters:
 
 Result:
     Rolls the given number of dice and produces the damage and effects dealt.
+
+    React to the resulting message with 🍀 to apply Miss Fortune to one die,
+    rerolling it. The reroller prioritises misses, then hits, and then finally
+    double-hits and specials (with equal priority). A die has been rerolled
+    already if it is surrounded in parenthesis.
 """,
 )
-async def damage(ctx: commands.Context, n_dice: str):
-    try:
-        message = ""
-        damage = 0
-        specials = 0
-        for _ in range(int(n_dice)):
-            die = DIE_ROLL_TO_DMG_DIE[random.randint(1, 6)]
-            message += str(DMG_DIE_TO_EMOJI[die])
-            damage += DMG_DIE_TO_DAMAGE[die]
-            if die == DamageDie.SPECIAL:
-                specials += 1
-        message += "\nTotal: **%s** damage and **%s** effect activations!" % (
-            damage,
-            specials,
-        )
-        await ctx.send(message)
+async def damage(ctx: commands.Context, *raw_n_dice: str):
+    if len(raw_n_dice) != 1:
+        await ctx.send("Error: expected 1 argument, got %s" % len(raw_n_dice))
+        return
 
+    try:
+        n_dice = int(raw_n_dice[0])
+    except ValueError:
+        await ctx.send("Error: argument was not a number: %s" % raw_n_dice)
+        return
+
+    try:
+        dice = DamageDice(n_dice)
+        msg: discord.Message = await ctx.send(dice)
+        message_to_damage[msg.id] = dice
+        await msg.add_reaction(FOUR_LEAF_CLOVER)
     except BaseException as e:
         try:
             await ctx.send("An internal error occured. Sorry!")
         except BaseException:
             pass
         raise e
+
+
+class Check:
+    def __init__(
+        self,
+        vs: typing.Optional[int] = None,
+        diff: typing.Optional[int] = None,
+        risk: int = 1,
+        tag: typing.Optional[int] = None,
+        dice: int = 2,
+    ) -> None:
+        self.vs = vs
+        self.diff = diff
+        self.risk = risk
+        self.tag = tag
+        self.dice = [CheckDie(self) for _ in range(dice)]
+        self.refresh_info()
+
+    def refresh_info(self):
+        self.complications = sum(1 for die in self.dice if die.complication)
+        self.successes = (
+            None
+            if self.vs is None
+            else sum(die.effective_successes for die in self.dice)
+        )
+        self.success = (
+            None
+            if self.diff is None or self.successes is None
+            else self.successes >= self.diff
+        )
+
+    def miss_fortune(self):
+        nonrerolled = [die for die in self.dice if not die.rerolled]
+        if len(nonrerolled) == 0:
+            return
+        lowest_die = max(nonrerolled, key=lambda die: die.roll)
+        lowest_die.reroll(True)
+        self.refresh_info()
+
+    def __repr__(self) -> str:
+        message = "Roll: %s\n\n" % " ".join(str(die) for die in self.dice)
+        if self.vs is not None:
+            message += "**%s** successes!\n" % self.successes
+        if self.complications > 0:
+            message += "**%s** complications occured.\n" % self.complications
+        if self.diff is not None:
+            message += (
+                "Test **"
+                + ("passed" if self.successes >= self.diff else "failed")
+                + "**!\n"
+            )
+            if self.successes > self.diff:
+                message += "**%s** AP generated." % (self.successes - self.diff)
+        return message
+
+
+message_to_check: typing.Dict[int, Check] = {}
+
+
+class CheckDie:
+    def __init__(self, check: Check) -> None:
+        self.check = check
+        self.reroll(False)
+
+    def reroll(self, rerolled: bool):
+        self.rerolled = rerolled
+        self.roll = random.randint(1, 20)
+        self.complication = self.roll > 20 - self.check.risk
+        self.critical = self.roll == 1 or (
+            self.check.tag is not None and self.roll <= self.check.tag
+        )
+        self.success = False if self.check.vs is None else (self.roll <= self.check.vs)
+
+    def __repr__(self) -> str:
+        result = str(self.roll)
+        if self.rerolled:
+            result = "*%s*" % result
+        if self.success:
+            result = "**%s**" % result
+        if self.critical:
+            result = "__%s__" % result
+        if self.complication:
+            result = "~~%s~~" % result
+        return result
+
+    @property
+    def effective_successes(self) -> int:
+        return 0 if not self.success else (2 if self.critical else 1)
 
 
 @client.command(
@@ -110,74 +261,77 @@ Parameters:
           Specify for <n> the value of your skill.
 
 Result:
-    Performs a skill check. Procuces success, complication,
+    Performs a skill check. Produces success, complication,
     pass/fail, and AP information.
+
+    React to the resulting message with 🍀 to apply Miss Fortune to one die,
+    rerolling it. A die has been rerolled already if it is italisized.
 """,
 )
 async def check(ctx: commands.Context, *args_):
     args = list(args_)
 
-    async def get_arg(name: str, default=None):
+    async def get_arg(name: str, default, type=lambda x: x):
         for i, arg in tuple(enumerate(args)):
             if arg == name:
                 if i < len(args) - 1:
                     result = args[i + 1]
                     del args[i + 1], args[i]
-                    return result
+                    return type(result)
                 else:
                     await ctx.send("error: argument %s needs a value" % name)
                     raise ValueError()
         return default
 
-    vs = int(await get_arg("vs", "-1"))
-    diff = int(await get_arg("diff", "-1"))
-    risk = int(await get_arg("risk", "1"))
-    tag = int(await get_arg("tag", "-1"))
+    vs = await get_arg("vs", None, int)
+    diff = await get_arg("diff", None, int)
+    risk = await get_arg("risk", 1, int)
+    tag = await get_arg("tag", None, int)
     if not args:
         dice = 2
     else:
         dice = int(args[0])
 
-    dice_rolled = [random.randint(1, 20) for _ in range(dice)]
-    message = "Roll: "
-    crit_succs = 0
-    complications = 0
-    succs = 0
-    first = True
+    check = Check(vs, diff, risk, tag, dice)
+    msg: discord.Message = await ctx.send(check)
+    message_to_check[msg.id] = check
+    await msg.add_reaction(FOUR_LEAF_CLOVER)
 
-    for die in dice_rolled:
-        if first:
-            first = False
-        else:
-            message += ", "
 
-        if die == 1:
-            crit_succs += 1
-            succs += 1
-        if die > 20 - risk:
-            complications += 1
-        if vs >= 0 and die <= vs:
-            succs += 1
-        if die != 1 and tag >= 0 and die <= tag:
-            crit_succs += 1
-            succs += 1
+@client.event
+async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
+    if payload.user_id == client.user.id:
+        return
 
-        if vs >= 0 and die <= vs:
-            message += "**"
-        message += str(die)
-        if vs >= 0 and die <= vs:
-            message += "**"
+    # for !damage
+    if payload.message_id in message_to_damage:
+        e: discord.PartialEmoji = payload.emoji
+        if not e.is_unicode_emoji():
+            return
+        if str(e) != FOUR_LEAF_CLOVER:
+            return
+        dice = message_to_damage[payload.message_id]
+        dice.miss_fortune()
+        channel = await client.fetch_channel(payload.channel_id)
+        if not isinstance(channel, discord.abc.Messageable):
+            return
+        msg: discord.Message = await channel.fetch_message(payload.message_id)
+        await msg.edit(content=dice)
 
-    message += "\n\n"
-    if vs >= 0:
-        message += "**%s** successes!\n" % succs
-    if complications > 0:
-        message += "**%s** complications occured.\n" % complications
-    if diff >= 0:
-        message += "Test **" + ("passed" if succs >= diff else "failed") + "**!\n"
-        if succs > diff:
-            message += "**%s** AP generated.\n" % (succs - diff)
-    await ctx.send(message)
+    # for !check
+    if payload.message_id in message_to_check:
+        e: discord.PartialEmoji = payload.emoji
+        if not e.is_unicode_emoji():
+            return
+        if str(e) != FOUR_LEAF_CLOVER:
+            return
+        check = message_to_check[payload.message_id]
+        check.miss_fortune()
+        channel = await client.fetch_channel(payload.channel_id)
+        if not isinstance(channel, discord.abc.Messageable):
+            return
+        msg: discord.Message = await channel.fetch_message(payload.message_id)
+        await msg.edit(content=check)
 
 
 @client.command(
